7023f4f22a1a668ef036201d7da4191d63d85847
codefulTM <nguyentuanminh435@gmail.com>
2025-11-22 15:19:58 +0700

    updated cors config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: [
+      configService.get<string>('FRONTEND_URL') || 'http://localhost:8000',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  });
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
