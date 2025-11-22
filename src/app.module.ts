8688a2d7d23fbba8596271e8ba4679067eb044d9
codefulTM <nguyentuanminh435@gmail.com>
2025-11-22 21:51:54 +0700

    initialized deck module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeckModule } from './deck/deck.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [ConfigService],
     }),
     AuthModule,
+    DeckModule,
   ],
   controllers: [AppController],
   providers: [AppService],
